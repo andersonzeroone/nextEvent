58e5c8da998bd30665e82547b63b8b8feb825d21
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-26 15:18:42 -0300

    list all events by firebase

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,15 @@
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data'
+import { getAllEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list';
 
-import EventsSearch from '../../components/events/events-search'
-function AllEventsPage() {
+import EventsSearch from '../../components/events/events-search';
+
+function AllEventsPage(props) {
 
   const router = useRouter();
 
-  const events = getAllEvents();
+  const { events } = props
 
   function findEventHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -22,6 +23,18 @@ function AllEventsPage() {
       <EventList items={events} />
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  }
 }
 
 export default AllEventsPage;
