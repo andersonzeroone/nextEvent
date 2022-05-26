e87f3a8fc445050c2ec7978afde670a9deeda8bd
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-26 16:06:34 -0300

    add head in homePage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { getFeaturedEvents } from './../helpers/api-util'
 import EventList from '../components/events/event-list';
+import Head from 'next/head';
 
 
 function HomePage(props) {
@@ -8,6 +9,13 @@ function HomePage(props) {
 
   return (
     <div>
+      <Head>
+        <title>NextJs events</title>
+        <meta
+          name='description'
+          content='find a lot of great events'
+        />
+      </Head>
       <EventList items={events} />
     </div>
   )
