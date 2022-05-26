f3d307efed5ff5a5b129f0988a60cdce064a8759
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-26 14:44:05 -0300

    view data the firebase

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -19,7 +19,7 @@ export async function getAllEvents() {
 
 }
 
-export function getFeaturedEvents() {
+export async function getFeaturedEvents() {
   const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
 }
