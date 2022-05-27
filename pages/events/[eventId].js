b060cad207174376a0e5f302f2a216e89f5f1dd8
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-27 11:24:55 -0300

    add inputs the comment in front end

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,28 +1,31 @@
-import EventSummary from '../../components/event-detail/event-summary';
-import { getEventById, getAllEvents, getFeaturedEvents } from '../../helpers/api-util';
+import { Fragment } from 'react';
 import Head from 'next/head';
 
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
+import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
-
 import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+import Comments from '../../components/input/comments';
 
-import ErrorAlert from '../../components/ui/error-alert'
-
-function EventsDetailPage(props) {
-
+function EventDetailPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
-    return <ErrorAlert>Not found Event!</ErrorAlert>
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
   }
-  return (
-    <>
 
+  return (
+    <Fragment>
       <Head>
         <title>{event.title}</title>
         <meta
           name='description'
-          content='find a lot of great events'
+          content={event.description}
         />
       </Head>
       <EventSummary title={event.title} />
@@ -32,27 +35,25 @@ function EventsDetailPage(props) {
         image={event.image}
         imageAlt={event.title}
       />
-
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
-    </>
-  )
+      <Comments eventId={event.id} />
+    </Fragment>
+  );
 }
 
 export async function getStaticProps(context) {
-
   const eventId = context.params.eventId;
-
-  console.log(eventId)
 
   const event = await getEventById(eventId);
 
   return {
     props: {
       selectedEvent: event
-    }
-  }
+    },
+    revalidate: 30
+  };
 }
 
 export async function getStaticPaths() {
@@ -60,11 +61,10 @@ export async function getStaticPaths() {
 
   const paths = events.map(event => ({ params: { eventId: event.id } }));
 
-
   return {
     paths: paths,
     fallback: 'blocking'
-  }
+  };
 }
 
-export default EventsDetailPage;
+export default EventDetailPage;
