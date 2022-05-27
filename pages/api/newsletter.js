f1e322c463146dcb20b509cf81fe676512bc287f
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-27 11:30:07 -0300

    connection input the register  with mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,3 +1,4 @@
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
 
 async function handler(req, res) {
   if (req.method === 'POST') {
@@ -5,6 +6,23 @@ async function handler(req, res) {
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address.' });
+      return;
+    }
+
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+
+    try {
+      await insertDocument(client, 'newsletter', { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
       return;
     }
 
