48cb3bc7beb2ef27073c9660470b8ad66b1e2e64
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-27 11:52:06 -0300

    add context in layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,15 +1,27 @@
-import MainHeader from "./main-header";
+import { Fragment, useContext } from 'react';
+
+import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
-  return (
-    <>
-      <MainHeader />
-      <main>
-        {props.children}
-      </main>
+  const notificationCtx = useContext(NotificationContext);
 
-    </>
-  )
+  const activeNotification = notificationCtx.notification;
+
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
+    </Fragment>
+  );
 }
 
 export default Layout;
